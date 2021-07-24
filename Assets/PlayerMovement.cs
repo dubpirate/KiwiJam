37f3bbb761f54ac7e468c77f92298b3df5118618
Spaxsta <devonkennedynz@yahoo.co.nz>
2021-07-24 12:20:07 +1200

    Added cube movement
    Added cube movement and rotate and collisions

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,30 +6,36 @@
  {
 
       public float speed = 1.5f;
-
+      public Rigidbody rigid;
+      public float rotation = 90f;
         void Start()
         {
 
         }
          void Update ()
          {
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.E) && transform.position.x > -720)
             {
                 transform.position += Vector3.left * speed * Time.deltaTime;
             }
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.Q) && transform.position.x < 540)
             {
                 transform.position += Vector3.right * speed * Time.deltaTime;
             }
-            
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.position += Vector3.up * speed * Time.deltaTime;
+
+            if (Input. GetKeyDown ("a")) {
+            transform. Rotate (0, rotation, 0);
             }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.position += Vector3.down * speed * Time.deltaTime;
+            if (Input. GetKeyDown ("d")) {
+            transform. Rotate (0, -(rotation), 0);
             }
-            
+
+            if (Input. GetKeyDown ("w")) {
+            transform. Rotate (rotation, 0, 0);
+            }
+            if (Input. GetKeyDown ("s")) {
+            transform. Rotate ((rotation), 0, 0);
+            }
+
         }
  }
